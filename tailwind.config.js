a6b29de86ec756bd6d449e508e20c3a9272b95e4
Ravin Samudika <raveensamudika@gmail.com>
2024-04-16 18:44:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import flowbite from "flowbite-react/tailwind";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite.plugin()],
+};
