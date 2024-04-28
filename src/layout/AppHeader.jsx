57baba4404d57b1c33697d26c45d17968033a37a
Ravin Samudika <raveensamudika@gmail.com>
2024-04-28 23:51:40 +0530

    set like UI

--- src/layout/AppHeader.jsx
+++ src/layout/AppHeader.jsx
@@ -15,6 +15,7 @@ import {
 import NavLogo from "../assets/QfitLogo/favicon.svg";
 
 function AppHeader() {
+  const isLoginPage = location.pathname === "/login";
   return (
     <Navbar fluid rounded className="bg-nav-cream">
       <NavbarBrand href="">
@@ -30,26 +31,29 @@ function AppHeader() {
             <DarkThemeToggle />
           </Flowbite>
         </div>
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded
-            />
-          }>
-          <DropdownHeader>
-            <span className="block text-sm">Name</span>
-            <span className="block truncate text-sm font-medium">Email</span>
-          </DropdownHeader>
-          <DropdownItem>Dashboard</DropdownItem>
-          <DropdownItem>Settings</DropdownItem>
-          <DropdownItem>Earnings</DropdownItem>
-          <DropdownDivider />
-          <DropdownItem>Sign out</DropdownItem>
-        </Dropdown>
+
+        {!isLoginPage && (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+              />
+            }>
+            <DropdownHeader>
+              <span className="block text-sm">Name</span>
+              <span className="block truncate text-sm font-medium">Email</span>
+            </DropdownHeader>
+            <DropdownItem>Dashboard</DropdownItem>
+            <DropdownItem>Settings</DropdownItem>
+            <DropdownItem>Earnings</DropdownItem>
+            <DropdownDivider />
+            <DropdownItem>Sign out</DropdownItem>
+          </Dropdown>
+        )}
         <NavbarToggle />
       </div>
     </Navbar>
