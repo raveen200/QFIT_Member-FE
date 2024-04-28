eafb22702e3e1bf59639154b91b1c50b8dc00f49
Ravin Samudika <raveensamudika@gmail.com>
2024-04-29 01:05:00 +0530

    set userlist Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import LoginPage from "./pages/LoginPage";
+import UserListPage from "./pages/UserListPage";
 
 import { Suspense } from "react";
 
@@ -11,7 +12,7 @@ function App() {
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
-            
+            <Route path="/" element={<UserListPage />} />
           </Routes>
         </Suspense>
       </AppLayout>
