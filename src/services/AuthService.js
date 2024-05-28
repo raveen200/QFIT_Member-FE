38e41f08140d8ae869aa091bbfaff7617cde9d82
Ravin Samudika <raveensamudika@gmail.com>
2024-05-28 15:49:08 +0530

    update

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -4,7 +4,6 @@ import authApi from "../configs/Auth";
 export const login = async (data) => {
   try {
     const response = await authApi.post(AUTH_CONSTANTS.LOGIN, data);
-    console.log(response);
     return response.data;
   } catch (error) {
     throw error;
